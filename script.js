65b2a1a3483dbd1cf1145503a6956a56a753ce08
Yash pathak <assush698@gmail.com>
2023-09-08 23:57:39 +0530

    added logos

--- script.js
+++ script.js
@@ -1,39 +1,71 @@
-const logos = [
+// const logos = [
 
-    'images/js.png',
-    'images/mysql.png',
-    'images/python.png'
-    , "images/0001.png"
-    , "images/0002.jpg"
-    , "images/0003.jpg"
-    , "images/0004.jpg"
-    , "images/0005.jpg"
-    , "images/0006.jpg"
-    , "images/0007.jpg"
-    , "images/0008.jpg"
-    , "images/0009.jpg"
-    , "images/0010.jpg"
-    , "images/0011.jpg"
-    , "images/0012.jpg"
-    , "images/0013.jpg"
-    , "images/0014.jpg"
-    , "images/0015.jpg"
-    , "images/0016.jpg"
-    , "images/0017.jpg"
-    , "images/0018.jpg"
-    , "images/0019.jpg"
-    , "images/0020.jpg"
-    , "images/0021.jpg"
-    , "images/0022.jpg"
-    , "images/0023.jpg"
-    , "images/0024.jpg"
-    // , "images/0024.jpg"
+//     'images/js.png',
+//     'images/mysql.png',
+//     'images/python.png'
+//     , "images/0001.png"
+//     , "images/0002.jpg"
+//     , "images/0003.jpg"
+//     , "images/0004.jpg"
+//     , "images/0005.jpg"
+//     , "images/0006.jpg"
+//     , "images/0007.jpg"
+//     , "images/0008.jpg"
+//     , "images/0009.jpg"
+//     , "images/0010.jpg"
+//     , "images/0011.jpg"
+//     , "images/0012.jpg"
+//     , "images/0013.jpg"
+//     , "images/0014.jpg"
+//     , "images/0015.jpg"
+//     , "images/0016.jpg"
+//     , "images/0017.jpg"
+//     , "images/0018.jpg"
+//     , "images/0019.jpg"
+//     , "images/0020.jpg"
+//     , "images/0021.jpg"
+//     , "images/0022.jpg"
+//     , "images/0023.jpg"
+//     , "images/0024.jpg"
+//     // , "images/0024.jpg"
 
-];
+// ];
 
 
 
 // Function to shuffle an array (Fisher-Yates algorithm)
+const logos = [
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/js.png',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/mysql.png',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/python.png',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0001.png',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0002.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0003.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0004.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0005.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0006.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0007.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0008.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0009.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0010.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0011.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0012.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0013.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0014.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0015.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0016.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0017.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0018.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0019.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0020.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0021.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0022.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0023.jpg',
+    'https://raw.githubusercontent.com/yash9111/TechBingo/master/images/0024.jpg'
+
+];
+
+
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
